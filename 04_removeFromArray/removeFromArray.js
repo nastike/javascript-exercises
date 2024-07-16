6749c0ef3d73a1fcbf72cf6c292dc0c72ed1c267
nastike <subheshjoshi@yahoo.com>
2024-07-16 17:43:00 +0545

    Remove from array test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = (inputArray, ...args)=>{
+    
+    let inputArrCopy = [...inputArray];
+    // console.log(inputArrCopy)
+    
+    
+   args.forEach(argument => {
+        
+        inputArray.forEach(element => {
+            
+            if(argument === element){
+                
+                let indexOfArgInArr = inputArrCopy.indexOf(argument);
+                
+                inputArrCopy.splice(indexOfArgInArr,1)
+               
+            }
+        });
+   });
+   return inputArrCopy;
+}
+console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4))
 // Do not edit below this line
 module.exports = removeFromArray;
