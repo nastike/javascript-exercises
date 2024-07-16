f2e6c4c937049f442727f2b22cb33cb830f75763
nastike <subheshjoshi@yahoo.com>
2024-07-16 16:08:11 +0545

    Reverse String Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const arrayOfStringGiven = text.split('');
+    let newReversedString = '';
+    for (let i = (arrayOfStringGiven.length-1); i >= 0; i-- )
+        {
+        newReversedString+= arrayOfStringGiven[i];
+        
+    }
+    return newReversedString
 };
-
+// reverseString('hello')
 // Do not edit below this line
 module.exports = reverseString;
